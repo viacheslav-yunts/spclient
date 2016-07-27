be0bd35c314cd35f7dd5830213aa2ea1b22de669
V.Yuntsevich <none@none.com>
2016-07-27 17:59:05 +0300

    create multi request v1.1

--- src/MultiRequest.php
+++ src/MultiRequest.php
@@ -3,15 +3,46 @@ namespace Sap\Odatalib;
 
 
 use Sap\Odatalib\IRequest;
+use Sap\Odatalib\SingleRequest;
 class MultiRequest implements IRequst
 {
-    private $_arr_requests = array();
-    
-    public function add(SingleRequest $single_request)
+    private $_requests = array();
+
+    public function add(SingleRequest $single_request, $key = null) 
     {
-        $this->_arr_requests[] = $single_request;
+        if ($key == null) {
+            $this->_requests[] = $obj;
+        } else {
+            if (isset($this->_requests[$key])) {
+                throw new \Exception("Key $key already in use.");
+            } else {
+                $this->_requests[$key] = $obj;
+            }
+        }
     }
-    
+
+    public function delete($key) 
+    {
+        if (isset($this->_requests[$key])) {
+            unset($this->_requests[$key]);
+        } else {
+            throw new \Exception("Invalid key $key.");
+        }
+    }
+
+    public function get($key)
+    {
+        if (isset($this->_requests[$key])) {
+            return $this->_requests[$key];
+        } else {
+            throw new \Exception("Invalid key $key.");
+        }
+    }
+
+    public function keyExists($key) {
+        return isset($this->_requests[$key]);
+    }
+
     public function execute()
     {
         
