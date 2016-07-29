313d0c01ba04d5894e4ee6f050894e8632c45987
V.Yuntsevich <none@none.com>
2016-07-29 14:17:26 +0300

    batch sub request v3

--- src/BatchRequest.php
+++ src/BatchRequest.php
@@ -4,10 +4,52 @@ namespace Sap\Odatalib;
 
 use Sap\Odatalib\IRequest;
 use Sap\Odatalib\SingleRequest;
+use Sap\Odatalib\BatchSubRequest;
 class BatchRequest extends SingleRequest implements IRequest
 {
-    public $arr_requests = array();
+    public $_requests = array();
 
+    public function add(BatchSubRequest $single_request, $key = null) 
+    {
+        if ($key == null) {
+            $this->_requests[] = $single_request;
+        } else {
+            if (isset($this->_requests[$key])) {
+                throw new \Exception("Key $key already in use.");
+            } else {
+                $this->_requests[$key] = $single_request;
+            }
+        }
+    }
+
+    public function delete($key) 
+    {
+        if (isset($this->_requests[$key])) {
+            unset($this->_requests[$key]);
+        } else {
+            throw new \Exception("Invalid key $key.");
+        }
+    }
+
+    public function get($key)
+    {
+        if (isset($this->_requests[$key])) {
+            return $this->_requests[$key];
+        } else {
+            throw new \Exception("Invalid key $key.");
+        }
+    }
+
+    public function keyExists($key) {
+        return isset($this->_requests[$key]);
+    }
+    
+    
+    
+    
+    
+    
+    
     /**
     * function buildQuery()
     * 
