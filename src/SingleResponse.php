3a2e9959dc9cd2b8b1faf103bb4e31217483ff13
V.Yuntsevich <none@none.com>
2016-08-03 16:12:31 +0300

    response add messages

--- src/SingleResponse.php
+++ src/SingleResponse.php
@@ -6,7 +6,7 @@ class SingleResponse
 {
     private $_http_code = 200;
     private $_http_text = '';
-    private $_error  = array();
+    private $_messages  = array();
     private $_headers = array();
     private $_body = '';
 
@@ -32,7 +32,9 @@ class SingleResponse
             }
         }
 
-        $this->_body = $body;
+        $this->_body = ResponseBodyHandler :: parse($this->getHeader('content-type'), $this->_body);
+
+        $this->_initMessages();
     }
 
     public function getHttpCode()
@@ -56,23 +58,97 @@ class SingleResponse
         return $this->_headers;
     }
 
-    public function getBody()
+    public function getData()
     {
         return $this->_body;
     }
 
-    public function getData()
-    {
-        return ResponseBodyHandler :: parse($this->getHeader('content-type'), $this->_body);
-    }
-
     public function hasError()
     {
-        return count($this->_error);
+        return count($this->_messages);
     }
 
     public function getMessages()
     {
-        return $this->_error;
+        return $this->_messages;
+    }
+
+    private function _initMessages()
+    {
+        $this->_messages = [];
+
+        // body messages 
+        if (!empty($this->_body->error)) {
+            if (isset($this->_body->error->innererror->errordetails)){
+                $this->setMessages($this->_body->error->innererror->errordetails);
+            } else {
+                $this->setMessages(
+                    (object) [
+                        'message'  => $this->_body->error->message->value,
+                        'severity'  => 'warning',
+                    ]
+                );
+            }
+        }
+
+        // response messages
+        if (! empty($this->_headers['sap-message'])) {
+            $messages =  json_decode($this->_headers['sap-message']);
+            if (! empty($messages->details)) {
+                $this->setMessages($messages->details, true);
+            } else {
+                $this->setMessages($messages, true);
+            }
+        }
+
+        // 404 internall error
+        if ($this->getHttpCode() >= 400 && $this->getHttpCode() < 500 && $this->getHttpCodeText()) {
+            if (empty($this->_messages)) {
+                $this->setMessages(
+                    (object) [
+                        'message'  => $this->getHttpCodeText(),
+                        'severity'  => 'warning',
+                    ]
+                );
+            }
+        }
+
+        // 500 internall error
+        if ($this->getHttpCode() != '200' && isset($this->_body->code[0]) && isset($this->_body->message[0])) {
+            $this->setMessages( 
+                (object) [
+                    'message'  => $this->_body->code[0]->__toString(),
+                    'severity'  => 'warning',
+                ]
+            );
+        }
+
+        // для 500 ошибки формируем критическое сообщение
+        if ($this->getHttpCode() == '500') {
+            $this->setMessages(
+                (object) [
+                    'message'  => 'В работе сервисов произошла исключительная ситуация, препятствующая возвращению результатов. В случае ее повторения обратитесь к оператору',   
+                    'severity'  => 'abort',
+                ]
+            );
+        }
+    }
+
+    public function setMessages($params, $url_encode=false )
+    {
+        if (is_object($params)) {
+            $this->_messages[] = array( 
+                'message'   =>  ($url_encode)?urldecode( str_replace("'","", $params->message) ):$params->message,
+                'type'      =>  $params->severity,
+            ); 
+        } elseif (is_array($params)) {
+            foreach($params as $m) {
+                $this->_messages[] = array( 
+                    'message'   =>  ($url_encode)?urldecode( str_replace("'","", $m->message) ):$m->message,
+                    'type'      =>  $m->severity,
+                ); 
+            }
+        }
+        return true;
     }
 }
