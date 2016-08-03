b4e12ab510da61be18a2c25edf5d9b2d350f914d
V.Yuntsevich <none@none.com>
2016-08-03 16:54:10 +0300

    response batch

--- src/BatchResponse.php
+++ src/BatchResponse.php
@@ -1,7 +1,8 @@
 <?php
 namespace Sap\Odatalib;
 
-
+require_once('./common/functions.php');
+use Sap\Odatalib\SingleResponse;
 class BatchResponse
 {
     private $_error = false;
@@ -39,41 +40,30 @@ class BatchResponse
     private function _prepBody($body)
     {
         $arr_single_responses = array();
+
         if ($content_type = $this->getHeader('content-type')) {
+
             preg_match('/boundary=(.*)$/', $content_type, $matches);
-            $boundary = $matches[1];
-            echo "<pre>"; print_r($boundary);
 
             // Fetch each part
-            $parts = array_slice(explode($boundary, $body), 1);
-            $data = array();
+            $parts = array_slice(explode($matches[1], $body), 1);
 
             foreach ($parts as $part) {
+                $part_headers = $part_body = '';
                 // If this is the last part, break
                 if ($part == "--\r\n") break;
 
                 // Separate content from headers
                 $part = ltrim($part, "\r\n");
-                list($raw_headers, $raw_body) = explode("\r\n\r\n", substr($part, strpos($part,'HTTP/1')), 2);
+                list($part_headers, $raw_body) = explode("\r\n\r\n", substr($part, strpos($part,'HTTP/1')), 2);
 
-                // Parse the headers list
-                $raw_headers = explode("\r\n", $raw_headers);
-                $headers = array();
-                foreach ($raw_headers as $header) {
-                    if (count($arr_header_info = explode(':', $header)) == 2 ) {
-                        $headers[strtolower($arr_header_info[0])] = ltrim($arr_header_info[1], ' ');
-                    }
-                }
-                echo "<pre>"; print_r($headers);
-                
                 $raw_body = explode("\r\n", $raw_body);
-                foreach ($raw_body as $str) {
-                    if (is_string($str) && is_array(json_decode($str, true)) && (json_last_error() == JSON_ERROR_NONE)) {
-                        $body = $str;
+                if (! empty($raw_body)) {
+                    foreach ($raw_body as $str) {
+                        if (isJson($str)) $part_body = $str;
                     }
                 }
-                echo "<pre>"; print_r($body);
-
+                $arr_single_responses[] = new SingleResponse($part_headers, $part_body);
             }
         }
         return $arr_single_responses;
