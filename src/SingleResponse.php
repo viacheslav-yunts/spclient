83288a706df3a4d33ca055de9664e66dc8fbbc51
V.Yuntsevich <none@none.com>
2016-08-03 14:43:29 +0300

    response data

--- src/SingleResponse.php
+++ src/SingleResponse.php
@@ -1,17 +1,26 @@
 <?php
 namespace Sap\Odatalib;
 
-
+use Sap\Odatalib\common\ResponseBodyHandler;
 class SingleResponse
 {
     private $_http_code = 200;
     private $_error  = array();
-    private $_header = '';
+    private $_headers = array();
     private $_body = '';
 
-    public function __construct($header, $body)
+    public function __construct($headers, $body)
     {
-        $this->_header = $header;
+        $this->_headers = array();
+
+        // Parse the headers list
+        $headers = explode("\r\n", $headers);
+        foreach ($headers as $header) {
+            if (count($arr_header_info = explode(':', $header)) == 2 ) {
+                $this->_headers[strtolower($arr_header_info[0])] = ltrim($arr_header_info[1], ' ');
+            }
+        }
+
         $this->_body = $body;
     }
 
@@ -20,9 +29,15 @@ class SingleResponse
         return $this->_http_code;
     }
 
-    public function getHeader()
+    public function getHeader($header_name)
     {
-        return $this->_header;
+        $header_name = strtolower(trim($header_name));
+        return isset($this->_headers[$header_name])?$this->_headers[$header_name]:FALSE;
+    }
+
+    public function getHeaders()
+    {
+        return $this->_headers;
     }
 
     public function getBody()
@@ -32,7 +47,7 @@ class SingleResponse
 
     public function getData()
     {
-        return json_decode($this->_body);
+        return ResponseBodyHandler :: parse($this->getHeader('content-type'), $this->_body);
     }
 
     public function hasError()
