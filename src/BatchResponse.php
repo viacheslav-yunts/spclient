67f88eb5ff9b20fe2961550def532e1808a95c13
V.Yuntsevich <none@none.com>
2016-08-03 16:32:51 +0300

    response batch

--- src/BatchResponse.php
+++ src/BatchResponse.php
@@ -4,14 +4,35 @@ namespace Sap\Odatalib;
 
 class BatchResponse
 {
+    private $_error = false;
     private $_http_code = 200;
-    private $_error  = array();
-    private $_header = '';
+    private $_http_text = '';
+    private $_messages  = array();
+    private $_headers = array();
     private $_body = '';
 
-    public function __construct($header, $body)
+    public function __construct($headers, $body)
     {
-        $this->_header = $header;
+        $this->_headers = array();
+
+        // Parse the headers list
+        $headers = explode("\r\n", $headers);
+        foreach ($headers as $header) {
+            if (count($arr_header_info = explode(':', $header)) == 2 ) {
+                $this->_headers[strtolower($arr_header_info[0])] = ltrim($arr_header_info[1], ' ');
+            } else {
+
+                $this->_headers['http_code'] = $header;
+
+                preg_match('#HTTP/\d+\.\d+ (\d+)#', $header, $matches);
+                if ( isset($matches[1])) {
+                    $this->_http_code = $matches[1];
+                    $this->_http_text = trim(str_replace($matches[0], '', $header));
+                }
+
+            }
+        }
+
         $this->_body = $body;
     }
 
