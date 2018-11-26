c26c82ad8548f936072b0d5a214086a29e6202a1
V.Yuntsevich <V.Yuntsevich>
2018-11-26 20:01:56 +0300

    bach request changes

--- src/BatchSubRequest.php
+++ src/BatchSubRequest.php
@@ -1,19 +1,22 @@
 <?php
 namespace Sap\Odatalib;
 
+use Sap\Odatalib\common\UrlEncodeTrait;
+
 /**
  * Class BatchSubRequest
  * @package Sap\Odatalib
  */
 class BatchSubRequest extends SingleRequest implements IRequest
 {
+    use UrlEncodeTrait;
 
     /**
      * @return null
      */
     public function constructUrl()
     {
-        return $this->getUrl() . '?' . $this->getSystemQueryOptionsToString();
+        return $this->getUrl() . '?' . $this->url_encode($this->getSystemQueryOptionsToString());
     }
 
     /**
