377e0768b19b6770df8567a2103ac5c26b99303d
V. Yuntsevich <V. Yuntsevich>
2020-08-19 15:15:01 +0300

    add HANA config

---
+++ src/config/BaseHanaConfig.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sap\Odatalib\config;
+
+class BaseHanaConfig extends BaseConfig
+{
+    /**
+     * SapConfig constructor.
+     *
+     * @param string $system
+     * @param string $connectionType
+     * @param string $configurationFile
+     */
+    public function __construct(string $system, string $connectionType, string $configurationFile)
+    {
+        $this->setSystem($system);
+        $this->setConnectionType($connectionType);
+        $this->setConfigurationFile($configurationFile);
+    }
+
+    /**
+     * @param array $parameters
+     *
+     * @return $this
+     *
+     * @throws \Exception
+     */
+    public function getConfig(array $parameters)
+    {
+        if ($this->isValid($parameters)) {
+            return $this;
+        }
+
+        throw new \Exception("Непройдена валидация конфигурационных параметров");
+    }
+}
